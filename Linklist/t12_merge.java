d4aadb32cb9f22d3aa910cdb59e72cba0ab165d6
Naini-magic <nraghuwanshi00401@gmail.com>
2025-02-17 23:24:31 +0530

    17th-feb

---
+++ Linklist/t12_merge.java
@@ -0,0 +1,51 @@
+
+
+class Solution {
+    // Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head) {
+        // add your code here
+        
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        Node mid = middle(head);
+        
+       Node lefthead = head , righthead = mid.next;
+       mid.next = null;
+       
+       
+      lefthead =  mergeSort(lefthead);
+      righthead = mergeSort(righthead);
+        
+    return  merge2(lefthead , righthead);
+    }
+    
+    static Node merge2(Node left , Node right){
+         if (left == null) return right;
+        if (right == null) return left;
+        
+        Node result;
+        
+        
+            if(left.data < right.data){
+             result = left;
+            result.next = merge2(left.next, right);
+            }
+            else{
+                result = right;
+            result.next = merge2(left, right.next);
+            }
+          return result;
+    }
+    static Node middle(Node head){
+          if (head == null) return head;
+        Node slow = head;
+        Node fast = head;
+         while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
