b5a0ea61032112f43bdd8462567138252f17b66e
Naini-magic <nraghuwanshi00401@gmail.com>
2025-03-04 21:26:54 +0530

    4-march

--- Linklist/t27.java
+++ Linklist/t27.java
@@ -2,6 +2,9 @@ public class t27 {
     public static void main(String[] args) {
         int a = 8 , b = 9;
         System.out.println(a+b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
 
     }
 }
