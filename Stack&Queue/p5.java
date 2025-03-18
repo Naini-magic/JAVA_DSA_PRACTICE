55e78849a4436f1773ddf103cb46233829a4bcdd
Naini-magic <nraghuwanshi00401@gmail.com>
2025-03-18 09:43:06 +0530

    18-march

--- Stack&Queue/p5.java
+++ Stack&Queue/p5.java
@@ -1,58 +1,130 @@
-public class p4_2Stack {
-   
-    private int maxSize;
-    private int[] stackArray;
-    private int top1, top2;
-    
-    public p4_2Stack(int s) {
-      maxSize = s;
-      stackArray = new int[maxSize];
-      top1 = -1;
-      top2 = maxSize; 
-  }
-  
+public class p5 {
+    // Node class for DLL
+    static class Node {
+        int data;
+        Node prev, next;
 
-    public boolean isFull() {
-        return (top1 + 1 == top2);
-     }
+        Node(int data) {
+            this.data = data;
+            this.prev = this.next = null;
+        }
+    }
 
-    public void push1(int j) {
-        if(isFull()){
-            System.out.println("Stack is full");
-            return;
+    // Stack class with DLL
+    static class Stack {
+        private Node head;
+        private Node mid;
+        private int size;
+
+        public Stack() {
+            head = mid = null;
+            size = 0;
         }
-       stackArray[++top1] = j;
-    }
-    public void push2(int k) {
-        if(isFull()){
-            System.out.println("Stack is full");
-            return;
+
+        // Push operation
+        void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+
+            if (head != null) {
+                head.prev = newNode;
+            }
+
+            head = newNode;
+            size++;
+
+            // Update mid pointer
+            if (size == 1) {
+                mid = head;
+            } else if (size % 2 != 0) { // Move mid back when odd elements
+                mid = mid.prev;
+            }
         }
-       stackArray[--top2] = k;
+
+        // Pop operation
+        public int pop() {
+            if (head == null) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+
+            int popData = head.data;
+            head = head.next;
+            size--;
+
+            // Update head's previous pointer
+            if (head != null) {
+                head.prev = null;
+            }
+
+            // Update mid pointer
+            if (size % 2 == 0 && mid != null) {
+                mid = mid.next; // Move mid forward when even elements
+            }
+
+            // If stack becomes empty, reset mid to null
+            if (size == 0) {
+                mid = null;
+            }
+
+            return popData;
+        }
+
+        // Find middle element
+        public int findMiddle() {
+            if (mid == null) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            return mid.data;
+        }
+
+        // Delete middle element
+        public void deleteMiddle() {
+            if (mid == null) {
+                System.out.println("Stack is empty");
+                return;
+            }
+
+            if (mid.prev != null) {
+                mid.prev.next = mid.next;
+            }
+            if (mid.next != null) {
+                mid.next.prev = mid.prev;
+            }
+
+            // Update mid pointer
+            if (size % 2 == 0) {
+                mid = mid.next; // Move mid forward when even elements
+            } else {
+                mid = mid.prev; // Move mid backward when odd elements
+            }
+
+            size--;
+
+            // If stack becomes empty, reset mid
+            if (size == 0) {
+                mid = null;
+            }
+        }
     }
-    public int pop1() {
-       if(top1 == -1){
-        System.out.println("Stack1 is empty");
-        return -1;
-       }
-       return stackArray[top1--];
-    }
-    public int pop2() {
-       if(top2 == maxSize){
-        System.out.println("Stack2 is empty");
-        return -1;
-       }
-       return stackArray[top2++];
-    }
-    
+
+    // Main function to test the stack
     public static void main(String[] args) {
-      p4_2Stack theStack = new p4_2Stack(10);
-      theStack.push1(1);
-      theStack.push2(2);
-      System.out.println(theStack.pop1()); // Output: 1
-      theStack.push1(3);
-      System.out.println(theStack.pop1()); // Output: 3
-      System.out.println(theStack.pop1()); // Output: -1 (stack1 is empty)
-  }
- }
+        Stack stack = new Stack();
 
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println("Middle element: " + stack.findMiddle()); // Output: 3
+
+        stack.deleteMiddle();
+        System.out.println("Middle element after deletion: " + stack.findMiddle()); // Output: 4
+
+        System.out.println("Popped: " + stack.pop()); // Output: 5
+        System.out.println("Middle element: " + stack.findMiddle()); // Output: 2
+    }
+}
