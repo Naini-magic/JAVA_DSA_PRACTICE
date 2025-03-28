cf689cbcf6fa696a7e3656b7d649e79ed92a0f05
Naini-magic <nraghuwanshi00401@gmail.com>
2025-03-28 22:24:37 +0530

    28-march

--- Stack&Queue/p19.java
+++ Stack&Queue/p19.java
@@ -2,35 +2,3 @@ public class p19 {
     
 }
 
-import java.util.Stack;
-
-public class p18_repeated {
-    public static boolean Repeated(String st) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : st.toCharArray()) {
-            if (c == '(') {
-                stack.push(c);
-            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                stack.push(c);
-            } else if (c == ')') {
-                if(stack.peek() == '('){
-                    return true;
-                }
-                while(stack.peek() == '+' || stack.peek() == '-' || stack.peek() == '*' || stack.peek() == '/' ){
-                    stack.pop();
-                }
-                stack.pop();
-            }
-        }
-        return false;
-    }
-
-    public static void main(String[] args) {
-        String s = "((a+b))";
-        System.out.println(Repeated(s));
-        s = "(a+(b)/c)";
-        System.out.println(Repeated(s));
-        s = "(a+b/c)";
-        System.out.println(Repeated(s));
-    }
-}
