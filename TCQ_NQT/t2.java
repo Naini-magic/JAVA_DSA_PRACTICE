ca0a1021d6e3b713c42e9eac15a651ff8bc1929f
Naini-magic <nraghuwanshi00401@gmail.com>
2025-04-13 00:33:57 +0530

    13-arpit

--- TCQ_NQT/t2.java
+++ TCQ_NQT/t2.java
@@ -1,33 +1,65 @@
-
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class t1 {
+public class t2{
+        
+    //factorial without multipication and divide
+    public static int multiply(int k , int j){
+        int result = 0;
+        for(int i = 0 ; i < j ; i++){
+            result += k;
+        }
+        return result;
+    }
+    public static int factorial(int a){
+        if(a == 1 || a == 0){
+            return 1;
+        }
+        return multiply(a , factorial(a-1));
+    }
     public static void main(String[] args) {
+ 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter length of arr");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        int count = 0;
-        int i = 0 ; 
-        int k = 0;
-        while(i < arr.length){
-            System.out.println("enter num : ");
-            int num = sc.nextInt();
-            if(num == 0){
-                count++;
-            }else{
-                arr[k] = num;
-                k++;
-            }
-            i++;
+        int T= sc.nextInt();
+        while(T-- > 0){
+            int N = sc.nextInt();
+            System.out.println(factorial(N));
         }
-        while (count > 0) {
-            arr[k] = 0;
-            k++;
-            count--;
-        }
-
-        System.out.println(Arrays.toString(arr));
+        sc.close();
     }
 }
+
+
+
+// without factorial 
+/*import java.util.Scanner;
+
+public class FactorialWithoutMulDiv {
+
+    // Function to multiply two numbers using only addition
+    static int multiply(int a, int b) {
+        int result = 0;
+        for (int i = 0; i < b; i++) {
+            result += a;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // number of test cases
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+
+            int result = 1;
+            for (int i = 2; i <= N; i++) {
+                result = multiply(result, i); // simulate result *= i
+            }
+
+            System.out.println(result);
+        }
+
+        sc.close();
+    }
+}
+ */
