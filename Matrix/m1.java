ec9fba47f463b990467d10344f14140329d02ed3
Naini-magic <nraghuwanshi00401@gmail.com>
2025-04-19 12:15:15 +0530

    19-april

---
+++ Matrix/m1.java
@@ -0,0 +1,16 @@
+package Matrix;
+
+import java.util.ArrayList;
+
+public class m1 {
+    public static ArrayList<Integer> Spiral(int arr[][]){
+      ArrayList<Integer> ans = new ArrayList<>();
+      for(int i = 0 ; i < arr.length ; i++){
+        
+      }
+    }
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3, 4} , {5, 6, 7, 8} , {9, 10, 11, 12} , {13, 14, 15, 16}};
+        System.out.println(Spiral(arr));
+    }
+}
