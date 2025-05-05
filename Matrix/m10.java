de8584f4ed0bbf2d5e7541a3b3c1956842205409
Naini-magic <nraghuwanshi00401@gmail.com>
2025-05-05 22:44:22 +0530

    5may

--- Matrix/m10.java
+++ Matrix/m10.java
@@ -1,3 +1,9 @@
 public class m10 {
-    
+    Diagram	Purpose
+Class Diagram	Shows classes, attributes, methods, relationships.
+Object Diagram	Snapshot of system instances at a moment in time.
+Component Diagram	Shows components (libraries, executables).
+Deployment Diagram	Physical architecture (nodes, devices).
+Package Diagram	Groups elements into packages.
+Composite Structure Diagram	Shows internal structure of a class.
 }
